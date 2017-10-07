29b53525e10a207d3e6989dde634f5635b9ad485
Nabeel Ahmed <chaudhrynabeelahmed@gmail.com>
2017-10-07 16:38:41 +0500

    odd_occurence redone

--- 2.oddoccurrency.py
+++ 2.oddoccurrency.py
@@ -22,9 +22,7 @@ def odd_occurency(odd_list):
 if __name__ == "__main__":
     odd_num = [9, 3, 9, 3, 9, 6, 9, 4, 4, 5, 6, 5, 10, 11, 11, 11, 10, 10, 11, 7]  # size 20
     print odd_occurency(odd_num)  # List has to have an odd number of elements!
-
     odd_num = [10, 9, 3, 9, 3, 9, 6, 9, 4, 4, 5, 6, 5, 10, 11, 11, 11, 10, 10, 11, 7]  # size 21
     print odd_occurency(odd_num)  # 7
-    
     odd_num = [10, 9, 3, 9, 3, 12, 9, 6, 9, 4, 4, 5, 6, 5, 10, 11, 11, 11, 10, 10, 11, 7, 7]  # size 23
     print odd_occurency(odd_num)  # 12 boundary case
