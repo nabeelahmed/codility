9c186989264ae4249b0f3516ac1365fb27d01f4a
Nabeel Ahmed <chaudhrynabeelahmed@gmail.com>
2017-10-07 16:53:22 +0500

    tap_equilibrium using while

--- 3.tapeequilibrium.py
+++ 3.tapeequilibrium.py
@@ -4,7 +4,19 @@ def tapeequilibrium(A):
         diff.add(abs(sum(A[:index]) - sum(A[index:])))
     return min(diff)
 
+def tape_equilibrium(num_list):
+    """
+
+    """
+    p = 1
+    while p < (len(num_list) - 1):
+        if abs(sum(num_list[:p]) - sum(num_list[p:])) == 1:  # tape equilibrium condition
+            return p
+        else:
+            p += 1
+    return p
+
 if __name__ == "__main__":
     A = [3, 1, 2, 4, 3]
-    answer = tapeequilibrium(A)
-    print answer
+    print tapeequilibrium(A)
+    print tape_equilibrium(A)
