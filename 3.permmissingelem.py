50dccc5689be495c619e5223797de50dc6b42ab0
Nabeel Ahmed <chaudhrynabeelahmed@gmail.com>
2017-10-08 14:26:55 +0500

    boundary cases for permmissingelem

--- 3.permmissingelem.py
+++ 3.permmissingelem.py
@@ -2,19 +2,20 @@
 
 def permissingelem(A):
     sorted_A = sorted(A)
-    print sorted_A
+    if sorted_A[0] != 1:  # boundary case - 1, first element is missing
+        return 1
     for index, elem in enumerate(sorted_A):
-        print sorted_A[index+1]
-        print sorted_A[index]
-        if not sorted_A[index+1] - sorted_A[index] == 1:
-            return sorted_A[index+1] - 1
-            #return sorted_A[index] + 1
+        if index < (len(sorted_A) - 1):
+            if not sorted_A[index+1] - sorted_A[index] == 1:
+                return sorted_A[index] + 1
         else:
-                pass
+            return sorted_A[index] + 1
 
 if __name__ == "__main__":
     # A = [2, 3, 1, 5]
     A = [2, 3, 1, 5, 6, 7, 8, 9, 10, 4, 12]
-    # A = [2, 3, 1, 5]
+    # A = [2, 3, 1, 5, 6, 7, 8, 9, 10, 4, 12]  # 11 is missing
+    A = [2, 3, 1, 5, 6, 7, 8, 9, 10, 4, 11]  # 12 - last elem is missing
+    A = [2, 3, 12, 5, 6, 7, 8, 9, 10, 4, 11]  # 1 - first elem is missing
     # A = [2, 3, 1, 5]
     print permissingelem(A)
